7598247f68f59fa1351289b5a4866277046cd31d
@khasanjonovich <khasanjonovich@gmail.com>
2021-11-02 00:10:15 +0900

    CRUD User

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+
 function App() {
   return (
     <Router>
@@ -12,6 +16,15 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
           </Route>
         </Switch>
       </div>
