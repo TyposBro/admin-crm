6e5e699488c9fdbd9e954a578fc4991c7c1b7c2f
@khasanjonovich <khasanjonovich@gmail.com>
2021-11-02 00:09:18 +0900

    React Router DOM

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
-
 function App() {
   return (
     <Router>
