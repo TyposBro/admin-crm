fc3b916e1559857a02beaf3d4c0634e4a7c30d5c
@khasanjonovich <khasanjonovich@gmail.com>
2021-11-02 00:03:23 +0900

    Info widgets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Topbar from "./components/topbar/Topbar.jsx";
 import Sidebar from "./components/sidebar/Sidebar.jsx";
+import Home from "./pages/home/Home.jsx";
 function App() {
   return (
     <>
       <Topbar />
       <div className="container">
         <Sidebar />
+        <Home />
       </div>
     </>
   );
