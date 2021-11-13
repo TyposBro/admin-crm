7db595faafdb3d57dc7d9b3b6b3144b7e09af514
Azizbek <khasanjonovich@gmail.com>
2021-11-13 22:47:32 +0900

    configure Context, AuthContext

---
+++ src/context/auth/AuthContext.js
@@ -0,0 +1,28 @@
+import reducers from "./AuthReducers";
+import actions from "./AuthActions";
+import { createContext, useReducer } from "react";
+
+const INITIAL_STATE = {
+  user: null,
+  isFetching: false,
+  error: false,
+};
+
+export const AuthConext = createContext(INITIAL_STATE);
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducers, INITIAL_STATE);
+
+  return (
+    <AuthConext.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthConext.Provider>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import AuthContext from "./context/auth/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContext>
+      <App />
+    </AuthContext>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
