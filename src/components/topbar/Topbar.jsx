35b7ac041531636ce913ddd27ce8d7f3d8bbd233
Azizbek <khasanjonovich@gmail.com>
2021-11-14 22:12:52 +0900

    Adding Logo

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./topbar.css";
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
-        <div className="topLeft">
-          <span className="logo">lamaadmin</span>
-        </div>
+        <Link to="/" className="topLeft">
+          <img src="/img/ju5t.png" alt="" />
+        </Link>
         <div className="topRight">
           <div className="topbarIconContainer">
             <NotificationsNone />
@@ -21,7 +22,11 @@ export default function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
         </div>
       </div>
     </div>
